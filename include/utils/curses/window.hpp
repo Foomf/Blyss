11476cee30e63ddb7abbbe48ccfc8a58fd2b67ce
Foomf <Foomf@Protonmail.com>
2019-09-22 23:59:55 -0700

    Use smart pointers

--- include/utils/curses/window.hpp
+++ include/utils/curses/window.hpp
@@ -3,6 +3,7 @@
 #include <cstdint>
 #include <memory>
 #include <string>
+#include <functional>
 
 #include "utils/curses/curses_state.hpp"
 
@@ -12,12 +13,11 @@ namespace blyss::utils::curses
 {
     class window
     {
-        //using curses_window_ptr = std::unique_ptr<_win, void(*)(const _win)>;
-        //using curses_window_ptr = std::unique_ptr<WINDOW>;
+        using curses_window_ptr = std::unique_ptr<_win, std::function<void(_win*)>>;
 
         std::shared_ptr<curses_state> curses_state_;
 
-        _win* win_ptr_;
+        curses_window_ptr win_ptr_;
 
     public:
         window(std::shared_ptr<curses_state> curses_state, std::int32_t nlines, std::int32_t ncols, std::int32_t begin_y, std::int32_t begin_x);
--- src/utils/curses/window.cpp
+++ src/utils/curses/window.cpp
@@ -6,18 +6,18 @@ namespace blyss::utils::curses
 {
     window::window(std::shared_ptr<curses_state> curses_state, std::int32_t nlines, std::int32_t ncols, std::int32_t begin_y, std::int32_t begin_x)
         : curses_state_{std::move(curses_state)}
-        , win_ptr_{newwin(nlines, ncols, begin_y, begin_x)}
+        , win_ptr_{curses_window_ptr(newwin(nlines, ncols, begin_y, begin_x), delwin)}
     {
     }
 
     void window::move_and_print(std::int32_t y, std::int32_t x, const std::string& str) const
     {
-        ::mvwprintw(win_ptr_, y, x, str.c_str());
+        ::mvwprintw(win_ptr_.get(), y, x, str.c_str());
     }
 
     std::int32_t window::get_char()
     {
-        return wgetch(win_ptr_);
+        return wgetch(win_ptr_.get());
     }
 
 
